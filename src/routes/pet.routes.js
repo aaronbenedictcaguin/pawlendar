cc2675cb882fb1f398679d1e1285ad854ef9d36f
Vhea C <aehv1215@gmail.com>
2026-06-30 00:13:12 +0800

    WIP: fixing bugs

--- src/routes/pet.routes.js
+++ src/routes/pet.routes.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const petController = require("../controllers/pet.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
+
+router.use(authMiddleware);
 
 router.post("/", petController.createPet);
 router.get("/", petController.getPets);
+router.get("/:id", petController.getPetById);
 router.put("/:id", petController.updatePet);
 router.delete("/:id", petController.deletePet);
 
